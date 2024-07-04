33c0536eeafb5d07a682a9b131ae7f65011546e0
Heemank Verma <heemankv@gmail.com>
2024-07-04 15:54:16 +0530

    update: getaddressesbylabel using rawRequest

--- main.go
+++ main.go
@@ -9,13 +9,23 @@ import (
 // Due to unupdated btcd rpc client,
 // Assumption: we will have to assume that the bitcoind is setup with only 1 wallet, whose name is preknown.
 
+// AddressInfo represents the structure of address information
+type AddressInfo struct {
+	Purpose string `json:"purpose"`
+	// Add other fields as needed
+}
+
 
 func main() {
+	// 1) Create an RPC Client.
 	client, err := bitcoindCreateClient()
 	if err != nil {
 		log.Fatalf("error creating new btc client: %v", err)
 	}
 	defer client.Shutdown()
+
+
+	// 2) Check available wallets, it should return the wallets that were setup.
 
 	// Prepare the method and parameters
 	method := "listwallets"
@@ -33,31 +43,87 @@ func main() {
 	// Print the result
 	fmt.Printf("Result: %+v\n", result)
 
-		// // iterate over accounts (map[string]btcutil.Amount) and write to stdout
-	// for label, amount := range accounts {
-	// 	log.Printf("%s: %s", label, amount)
-	// }
+	// 3) Get the addresses of the given label, by default use the first address.
 
-	// // prepare a sendMany transaction
-	// receiver1, err := btcutil.DecodeAddress("1someAddressThatIsActuallyReal", &chaincfg.MainNetParams)
-	// if err != nil {
-	// 	log.Fatalf("address receiver1 seems to be invalid: %v", err)
-	// }
-	// receiver2, err := btcutil.DecodeAddress("1anotherAddressThatsPrettyReal", &chaincfg.MainNetParams)
-	// if err != nil {
-	// 	log.Fatalf("address receiver2 seems to be invalid: %v", err)
-	// }
-	// receivers := map[btcutil.Address]btcutil.Amount{
-	// 	receiver1: 42,  // 42 satoshi
-	// 	receiver2: 100, // 100 satoshi
-	// }
+	// Prepare the method and parameters
+	method = "getaddressesbylabel"
+	params = []json.RawMessage{json.RawMessage(`"btcstaker"`)}
 
-	// // create and send the sendMany tx
-	// txSha, err := client.SendMany("some-account-label-from-which-to-send", receivers)
-	// if err != nil {
-	// 	log.Fatalf("error sendMany: %v", err)
-	// }
-	// log.Printf("sendMany completed! tx sha is: %s", txSha.String())
+	// Define a variable to hold the result
+	var result2 map[string]AddressInfo
+
+	// Call the generalized function
+	err = bitcoindCreateRawRequest(client, method, params, &result2)
+	if err != nil {
+		log.Fatalf("Error calling RPC method: %v", err)
+	}
+
+	// Print the result
+	for address, info := range result2 {
+		fmt.Printf("Address: %s, Purpose: %s\n", address, info.Purpose)
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	//  Staking Transaction Code helps
 	// accounts, err := client.FundRawTransaction()
